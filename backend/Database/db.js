270fc7fb6204aba9f13640aaa2c14dbe2a78d0e2
Mr.Shahid <shahid.gce@gmail.com>
2026-02-12 01:33:21 +0530

    adjusted

--- backend/Database/db.js
+++ backend/Database/db.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const uri =
   process.env.NODE_ENV === "production"
-    ? process.env.MONGO_URL // Atlas on Render
+    ? process.env.MONGO_DB // Atlas on Render
     : "mongodb+srv://khansaqeeb332_db_user:KrGPyEDLGxJBySSs@cluster1.xnkbbvy.mongodb.net/"; // local dev
 
 const MongoConnect = async () => {
@@ -10,6 +10,14 @@ const MongoConnect = async () => {
     if (!uri) {
       throw new Error("MONGO_URL is not defined");
     }
+    console.log(
+      "MONGO_URI starts with:",
+      typeof process.env.MONGO_DB === "string"
+        ? process.env.MONGO_DB.substring(0, 50) + "..."
+        : process.env.MONGO_DB,
+    );
+    console.log("Full length:", process.env.MONGO_DB?.length || "undefined");
+
     await mongoose.connect(uri);
     console.log("MongoDB connected");
   } catch (err) {
