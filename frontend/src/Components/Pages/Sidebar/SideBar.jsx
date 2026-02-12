5c53d153c418bbff0a19ab6d1f0ee38b0832e280
Mr.Shahid <shahid.gce@gmail.com>
2026-02-12 22:23:25 +0530

    finish all work

--- frontend/src/Components/Pages/Sidebar/SideBar.jsx
+++ frontend/src/Components/Pages/Sidebar/SideBar.jsx
@@ -1,7 +1,8 @@
 import { useState } from "react";
 import SidebarItem from "./SideBarItem";
-import { NAV_CONFIG } from "../../Custom/DropDown";
-import "./SideBar.css"
+import { NAV_CONFIG, NavLinks } from "../../Custom/DropDown";
+import "./SideBar.css";
+import { Link } from "react-router-dom";
 
 const SideBar = ({ isOpen, onClose }) => {
   const [openIndex, setOpenIndex] = useState(null);
@@ -15,6 +16,20 @@ const SideBar = ({ isOpen, onClose }) => {
       <button className="sidebar-close" onClick={onClose}>
         x
       </button>
+      <div className="container">
+        {NavLinks.map((nav) => (
+          <div className="nav" key={nav}>
+            <div>
+              <Link
+                to={`/${nav.toLowerCase().replace(/\s+/g, "")}`}
+                className="nav-link"
+              >
+                {nav}
+              </Link>
+            </div>
+          </div>
+        ))}
+      </div>
 
       <div className="sidebar_container">
         {NAV_CONFIG.map((nav, index) => (
